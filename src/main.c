7d12964bcbc70f2cab94ddde5f261b42872e8d92
Mike Burns <mike@mike-burns.com>
2015-10-31 02:58:07 +0100

    First spike at getting desktop entries shown
    A new direction: only show desktop entries, and make it easy to select
    them.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <gtk/gtk.h>
 
@@ -9,12 +10,15 @@ void	gui_warn(char *);
 
 int	run_cmd(const char *, char **);
 
+gboolean	print_cell(GtkTreeModel *, GtkTreePath *, GtkTreeIter *, gpointer);
+
 /*
  * A program runner.
  */
 int
 main(int argc, char *argv[])
 {
+	const gchar *const *dirs;
 	GtkWidget	*window, *box, *label, *entry;
 	GValue		 g_9 = G_VALUE_INIT;
 
@@ -46,11 +50,102 @@ main(int argc, char *argv[])
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	g_signal_connect(window, "response", G_CALLBACK(run_entry), entry);
 
+	/* add apps from desktop entries */
+	GtkListStore	*apps;
+	DIR		*dirp;
+	struct dirent	*dp;
+	char		*dir, *fn = NULL, *name = NULL;
+	size_t		 len;
+	GKeyFile	*key_file;
+	GError		*error;
+	GtkWidget	*apps_tree;
+	GtkTreeViewColumn	*name_col;
+
+	apps = gtk_list_store_new(1, G_TYPE_STRING);
+	for (dirs = g_get_system_data_dirs(); *dirs; dirs++) {
+		len = strlen(*dirs) + 14;
+		if ((dir = calloc(len, sizeof(char))) == NULL)
+			err(1, "calloc");
+		if (snprintf(dir, len, "%s/%s", *dirs, "applications") >= (int)len)
+			err(1, "snprintf");
+		if ((dirp = opendir(dir)) == NULL)
+			goto cont;
+
+		while ((dp = readdir(dirp)) != NULL)
+			if (dp->d_namlen > 8 &&
+			    strncmp(dp->d_name + dp->d_namlen - 8,
+				    ".desktop", 8) == 0) {
+				if ((fn = calloc(dp->d_namlen + len + 1,
+						    sizeof(char))) == NULL)
+					err(1, "calloc");
+				if (snprintf(fn, dp->d_namlen + len + 1,
+					    "%s/%s", dir, dp->d_name) >=
+				    dp->d_namlen + (int)len + 1)
+					err(1, "snprintf");
+
+				key_file = g_key_file_new();
+				if (!g_key_file_load_from_file(key_file,
+					    fn, G_KEY_FILE_NONE, &error))
+					errx(1, "%s", error->message);
+
+				name = g_key_file_get_locale_string(key_file,
+				    "Desktop Entry", "Name", NULL, &error);
+				if (name == NULL) {
+					warnx("g_key_file_get_locale_string: %s",
+					    error->message);
+					goto cont;
+				}
+
+				gtk_list_store_insert_with_values(
+				    apps, NULL, -1,
+				    0, name,
+				    -1);
+
+				free(name); name = NULL;
+				free(fn); fn = NULL;
+				g_key_file_free(key_file);
+			}
+
+cont:
+		free(name);
+		free(fn);
+
+		if (dirp && closedir(dirp) < 0)
+			err(1, "closedir");
+
+		free(dir);
+	}
+
+	gtk_tree_model_foreach(GTK_TREE_MODEL(apps), print_cell, NULL);
+	apps_tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(apps));
+	name_col = gtk_tree_view_column_new_with_attributes(
+	    "Name",
+	    gtk_cell_renderer_text_new(),
+	    "text", 0,
+	    NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(apps_tree), name_col);
+	gtk_box_pack_start(GTK_BOX(box), apps_tree, /* expand */ 1, /* fill */ 1,
+	    /* padding */ 3);
+	/* that was the app code */
+
 	gtk_widget_show_all(window);
 
 	gtk_main();
 
 	return 0;
+}
+
+gboolean
+print_cell(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
+{
+	GValue	value = G_VALUE_INIT;
+
+	gtk_tree_model_get_value(model, iter, 0, &value);
+
+	fprintf(stderr, "print_cell: %s\n", g_value_get_string(&value));
+
+	g_value_unset(&value);
+	return FALSE;
 }
 
 /*
