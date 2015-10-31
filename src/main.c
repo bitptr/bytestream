6f0f1d1bbd2a7a040116a558f26f0fd83dbf5eea
Mike Burns <mike@mike-burns.com>
2015-10-31 04:11:18 +0100

    Scrolling window and code readability
    It's not beautiful yet, but it's closer.

--- src/main.c
+++ src/main.c
@@ -10,7 +10,9 @@ void	gui_warn(char *);
 
 int	run_cmd(const char *, char **);
 
-gboolean	print_cell(GtkTreeModel *, GtkTreePath *, GtkTreeIter *, gpointer);
+GtkListStore	*collect_apps();
+GtkWidget	*apps_tree_new();
+void		 apps_list_insert_files(GtkListStore *, char *, DIR *, size_t);
 
 /*
  * A program runner.
@@ -18,8 +20,7 @@ gboolean	print_cell(GtkTreeModel *, GtkTreePath *, GtkTreeIter *, gpointer);
 int
 main(int argc, char *argv[])
 {
-	const gchar *const *dirs;
-	GtkWidget	*window, *box, *label, *entry;
+	GtkWidget	*window, *box, *label, *apps_tree, *scrollable;
 	GValue		 g_9 = G_VALUE_INIT;
 
 	gtk_init(&argc, &argv);
@@ -32,101 +33,22 @@ main(int argc, char *argv[])
 	    NULL,
 	    0,
 	    "_Close", GTK_RESPONSE_CLOSE,
-	    "_Run", GTK_RESPONSE_OK,
 	    NULL);
 	box = gtk_dialog_get_content_area(GTK_DIALOG(window));
-	label = gtk_label_new("Desktop program or full path and options.");
-	entry = gtk_entry_new();
+	label = gtk_label_new("Select program.");
+	scrollable = gtk_scrolled_window_new(NULL, NULL);
+	apps_tree = apps_tree_new();
 
-	gtk_box_pack_start(GTK_BOX(box), label, /* expand */ 1, /* fill */ 1,
-	    /* padding */ 3);
-	gtk_box_pack_start(GTK_BOX(box), entry, /* expand */ 1, /* fill */ 1,
-	    /* padding */ 3);
-
-	gtk_dialog_set_default_response(GTK_DIALOG(window), GTK_RESPONSE_OK);
-	gtk_entry_set_activates_default(GTK_ENTRY(entry), 1);
+	gtk_widget_set_size_request(window, 400, 300);
 	g_object_set_property(G_OBJECT(box), "margin", &g_9);
 
-	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-	g_signal_connect(window, "response", G_CALLBACK(run_entry), entry);
-
-	/* add apps from desktop entries */
-	GtkListStore	*apps;
-	DIR		*dirp;
-	struct dirent	*dp;
-	char		*dir, *fn = NULL, *name = NULL;
-	size_t		 len;
-	GKeyFile	*key_file;
-	GError		*error;
-	GtkWidget	*apps_tree;
-	GtkTreeViewColumn	*name_col;
-
-	apps = gtk_list_store_new(1, G_TYPE_STRING);
-	for (dirs = g_get_system_data_dirs(); *dirs; dirs++) {
-		len = strlen(*dirs) + 14;
-		if ((dir = calloc(len, sizeof(char))) == NULL)
-			err(1, "calloc");
-		if (snprintf(dir, len, "%s/%s", *dirs, "applications") >= (int)len)
-			err(1, "snprintf");
-		if ((dirp = opendir(dir)) == NULL)
-			goto cont;
-
-		while ((dp = readdir(dirp)) != NULL)
-			if (dp->d_namlen > 8 &&
-			    strncmp(dp->d_name + dp->d_namlen - 8,
-				    ".desktop", 8) == 0) {
-				if ((fn = calloc(dp->d_namlen + len + 1,
-						    sizeof(char))) == NULL)
-					err(1, "calloc");
-				if (snprintf(fn, dp->d_namlen + len + 1,
-					    "%s/%s", dir, dp->d_name) >=
-				    dp->d_namlen + (int)len + 1)
-					err(1, "snprintf");
-
-				key_file = g_key_file_new();
-				if (!g_key_file_load_from_file(key_file,
-					    fn, G_KEY_FILE_NONE, &error))
-					errx(1, "%s", error->message);
-
-				name = g_key_file_get_locale_string(key_file,
-				    "Desktop Entry", "Name", NULL, &error);
-				if (name == NULL) {
-					warnx("g_key_file_get_locale_string: %s",
-					    error->message);
-					goto cont;
-				}
-
-				gtk_list_store_insert_with_values(
-				    apps, NULL, -1,
-				    0, name,
-				    -1);
-
-				free(name); name = NULL;
-				free(fn); fn = NULL;
-				g_key_file_free(key_file);
-			}
-
-cont:
-		free(name);
-		free(fn);
-
-		if (dirp && closedir(dirp) < 0)
-			err(1, "closedir");
-
-		free(dir);
-	}
-
-	gtk_tree_model_foreach(GTK_TREE_MODEL(apps), print_cell, NULL);
-	apps_tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(apps));
-	name_col = gtk_tree_view_column_new_with_attributes(
-	    "Name",
-	    gtk_cell_renderer_text_new(),
-	    "text", 0,
-	    NULL);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(apps_tree), name_col);
-	gtk_box_pack_start(GTK_BOX(box), apps_tree, /* expand */ 1, /* fill */ 1,
+	gtk_container_add(GTK_CONTAINER(scrollable), apps_tree);
+	gtk_box_pack_start(GTK_BOX(box), label, /* expand */ 0, /* fill */ 1,
 	    /* padding */ 3);
-	/* that was the app code */
+	gtk_box_pack_start(GTK_BOX(box), scrollable, /* expand */ 1,
+	    /* fill */ 1, /* padding */ 3);
+
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
 	gtk_widget_show_all(window);
 
@@ -135,17 +57,112 @@ cont:
 	return 0;
 }
 
-gboolean
-print_cell(GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
+/*
+ * Return a GtkTreeView* populated with data from all desktop entries.
+ */
+GtkWidget *
+apps_tree_new()
 {
-	GValue	value = G_VALUE_INIT;
+	GtkWidget		*apps_tree;
+	GtkListStore		*apps;
+	GtkTreeViewColumn	*name_col;
 
-	gtk_tree_model_get_value(model, iter, 0, &value);
+	apps = collect_apps();
+	apps_tree = gtk_tree_view_new_with_model(GTK_TREE_MODEL(apps));
 
-	fprintf(stderr, "print_cell: %s\n", g_value_get_string(&value));
+	name_col = gtk_tree_view_column_new_with_attributes(
+	    "Name", gtk_cell_renderer_text_new(),
+	    "text", 0,
+	    NULL);
+	gtk_tree_view_append_column(GTK_TREE_VIEW(apps_tree), name_col);
 
-	g_value_unset(&value);
-	return FALSE;
+	gtk_tree_view_set_headers_visible(GTK_TREE_VIEW(apps_tree), FALSE);
+	gtk_tree_view_set_enable_search(GTK_TREE_VIEW(apps_tree), TRUE);
+	gtk_tree_view_set_search_column(GTK_TREE_VIEW(apps_tree), 0);
+
+	return apps_tree;
+}
+
+/*
+ * Return a GtkListStore* populated with data from all desktop entries.
+ */
+GtkListStore *
+collect_apps()
+{
+	GtkListStore	*apps;
+	DIR		*dirp;
+	char		*dir;
+	size_t		 len;
+	const gchar *const *dirs;
+
+	apps = gtk_list_store_new(1, G_TYPE_STRING);
+
+	for (dirs = g_get_system_data_dirs(); *dirs; dirs++) {
+		len = strlen(*dirs) + 14;
+		if ((dir = calloc(len, sizeof(char))) == NULL)
+			err(1, "calloc");
+		if (snprintf(dir, len, "%s/%s", *dirs, "applications") >= (int)len)
+			err(1, "snprintf");
+
+		if ((dirp = opendir(dir)) == NULL)
+			goto cont;
+
+		apps_list_insert_files(apps, dir, dirp, len);
+
+		if (dirp && closedir(dirp) < 0)
+			err(1, "closedir");
+
+cont:
+		free(dir);
+	}
+
+	return apps;
+}
+
+/*
+ * Insert all desktop files in a directory into the GtkListStore.
+ */
+void
+apps_list_insert_files(GtkListStore *apps, char *dir, DIR *dirp, size_t len)
+{
+	size_t		 len_fn;
+	char		*fn = NULL, *name = NULL;
+	struct dirent	*dp;
+	GKeyFile	*key_file = NULL;
+	GError		*error;
+
+	while ((dp = readdir(dirp)) != NULL) {
+		if (dp->d_namlen <= 8)
+			goto cont;
+		if (strncmp(dp->d_name + dp->d_namlen - 8, ".desktop", 8) != 0)
+			goto cont;
+
+		len_fn = dp->d_namlen + len + 1;
+		if ((fn = calloc(len_fn, sizeof(char))) == NULL)
+			err(1, "calloc");
+		if (snprintf(fn, len_fn, "%s/%s", dir, dp->d_name) >= (int)len_fn)
+			err(1, "snprintf");
+
+		key_file = g_key_file_new();
+		if (!g_key_file_load_from_file(key_file, fn, G_KEY_FILE_NONE, &error))
+			errx(1, "%s", error->message);
+
+		name = g_key_file_get_locale_string(key_file, "Desktop Entry",
+		    "Name", NULL, &error);
+		if (name == NULL) {
+			warnx("g_key_file_get_locale_string: %s",
+			    error->message);
+			goto cont;
+		}
+
+		gtk_list_store_insert_with_values(apps, NULL, -1, 0, name, -1);
+
+cont:
+		free(name);
+		free(fn);
+		if (key_file)
+			g_key_file_free(key_file);
+	}
 }
 
 /*
